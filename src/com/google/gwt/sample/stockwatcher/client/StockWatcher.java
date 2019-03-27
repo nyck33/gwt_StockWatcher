2dc21a3f7e00cf6a4fec38edd355a5e2098dc3cb
nobu <nobu.kim66@gmail.com>
2019-03-27 09:59:29 +0700

    event handling added, not done

--- src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -1,6 +1,14 @@
 package com.google.gwt.sample.stockwatcher.client;
 
+import java.util.ArrayList;
+
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -17,6 +25,7 @@ public class StockWatcher implements EntryPoint {
 	private TextBox newSymbolTextBox = new TextBox();
 	private Button addStockButton = new Button("Add");
 	private Label lastUpdatedLabel = new Label();
+	private ArrayList<String> stocks = new ArrayList<String>();
 	
 	/*Entry point method*/
 	
@@ -39,5 +48,58 @@ public class StockWatcher implements EntryPoint {
 		
 		//Move cursor focus to the input box.  
 		newSymbolTextBox.setFocus(true);
+		
+		//Listen for mouse events on the Add button.
+		addStockButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				addStock();
+			}
+		});
+		
+		//Listen for keyboard events in the input box
+		newSymbolTextBox.addKeyDownHandler(new KeyDownHandler() {
+			public void onKeyDown(KeyDownEvent event) {
+				if(event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					addStock();
+				}
+			}
+		});
 	}
-}
+
+	/*Add stock to FlexTable.  Executed when user clicks the addStockButton 
+	 * or presses enter in the newSymbolTextBox.
+	 */
+	private void addStock() {
+		final String symbol = newSymbolTextBox.getText().toUpperCase().trim();
+		newSymbolTextBox.setFocus(true);
+				
+		if(!symbol.matches("^[0-9A-Z\\.]{1,10}$")) {
+			Window.alert("'" + symbol + "' is not a valid symbol.");
+			newSymbolTextBox.selectAll();
+			return;
+		}	
+		
+		newSymbolTextBox.setText("");
+		
+		//don't add if already in table
+		if(stocks.contains(symbol))
+			return;
+		//TODO Add stock to table
+		int row = stocksFlexTable.getRowCount();
+		stocks.add(symbol);
+		stocksFlexTable.setText(row, 0, symbol);
+		//TODO Add a button to remove this stock from table
+		Button removeStockButton = new Button("x");
+		removeStockButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+			int removedIndex = stocks.indexOf(symbol);
+			stocks.remove(removedIndex);
+			stocksFlexTable.removeRow(removedIndex + 1);
+		}
+	});
+	stocksFlexTable.setWidget(row, 3, removeStockButton);
+		
+		//TODO get stock price
+		}
+		
+	}
